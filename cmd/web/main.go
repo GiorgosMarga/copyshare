fb100bd559148d40766ed2737e2562f31976a64a
GiorgosMarga <giorgosmarga509@gmail.com>
2023-10-02 21:20:57 +0300

    fixed init

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load(".env")
+// func init() {
+// 	err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// }
 
 func main() {
 	dsn := os.Getenv("DSN")
